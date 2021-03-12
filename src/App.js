7922f90831728d4ccb13ee365de2f273dce331a5
Balghari <najmbalghari13@gmail.com>
2021-03-12 19:20:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Najmuddin Balghari
+          Najmuddin Saqib Balghari 
         </p>
         <a
           className="App-link"
